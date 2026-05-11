b062dfd04dd117c13470b9ab82cc90c22106f56b
Hassan <hassan@gmail.com>
2026-05-11 20:33:37 +0600

    delete operation

--- src/app/destination/[id]/page.jsx
+++ src/app/destination/[id]/page.jsx
@@ -1,3 +1,4 @@
+import { DeleteAlert } from "@/components/DeleteAlert";
 import { EditModal } from "@/components/EditModal";
 import { Button } from "@heroui/react";
 import Image from "next/image";
@@ -18,7 +19,7 @@ const DestinationDetailsPage = async ({ params }) => {
     <div className="max-w-7xl mx-auto">
       <div className="flex  items-center gap-3 justify-end mt-5 mb-3">
         <EditModal destination={destination} />
-      
+         <DeleteAlert destination={destination}/>
       </div>
       <Image
         className="w-full h-100 object-cover"
